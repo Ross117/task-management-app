6f8fcdbdaf7d03c6253c5a9104ae17e5863878cd
Ross <RossMorran@hotmail.co.uk>
2018-12-03 19:49:55 +0000

    Add further styling to Task component

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -3,12 +3,19 @@ import "../css/Task.css";
 
 function Task(props) {
   const [ id, createdDt, title, desc, isCompleted, scheduledDt, priority ] = props.task;
+  const cleanedScheduledDt = (scheduledDt.value ? scheduledDt.value.substring(0, 10) : "");
   
+  // think Task should be a form made up of input eles to allow edits
+
   return (
-    <div>
-      <h2>{title.value}</h2>
-      <p>{desc.value}</p>
-      <p>{isCompleted.value} {scheduledDt.value} {priority.value}</p>
+    <div className="task">
+      <h2 className="task__title">{title.value}</h2>
+      <input className="task__isCompleted" type="checkbox" checked={isCompleted.value}/>
+      <p className="task__desc">{desc.value}</p>
+      <p>
+        <span className="task__scheduledDt">{cleanedScheduledDt}</span>
+        <span className="task__priority">{priority.value}</span>
+      </p>
     </div>
   );
 }
