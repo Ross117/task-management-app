586d7717b45d5f3a4952d73af6de27d4530c95c4
Ross <RossMorran@hotmail.co.uk>
2018-12-07 01:42:00 +0000

    Run npm build

---
+++ client/build/precache-manifest.c3ef1b2cdb1ab12be2cd831f2bfb438f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f06ba64c72351e06d260",
+    "url": "/static/js/main.f06ba64c.chunk.js"
+  },
+  {
+    "revision": "4fd3fa28b56715afcdf6",
+    "url": "/static/js/1.4fd3fa28.chunk.js"
+  },
+  {
+    "revision": "f06ba64c72351e06d260",
+    "url": "/static/css/main.491e5e90.chunk.css"
+  },
+  {
+    "revision": "1570197a043560a339da12bfe978cdf3",
+    "url": "/index.html"
+  }
+];
