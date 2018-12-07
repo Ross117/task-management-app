241d35064bdf371fb456ad63b38152ce1c7824ba
Ross <RossMorran@hotmail.co.uk>
2018-12-07 06:36:53 +0000

    Fix path bug in package.json

--- server/server.js
+++ server/server.js
@@ -19,10 +19,10 @@ app.post("/addTask/:taskTitle", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 }
 
