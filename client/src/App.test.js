6eaa35f34495858c9fba315944bfbe7a972bc9c3
Ross <RossMorran@hotmail.co.uk>
2019-10-24 00:28:19 +0100

    Add test to check that Task components are rendered

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,32 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import App from "./App";
 
-test("App renders", () => {
-  const app = shallow(<App/>);
+describe("the App", () => {
+
+  const app = mount(<App />);
+  const mockTask = [
+    { value: "", metadata: { colName: "task_id" } }, 
+    { value: "", metadata: { colName: "task_creation_dt" } }, 
+    { value: "", metadata: { colName: "task_title" } }, 
+    { value: "", metadata: { colName: "task_desc" } }, 
+    { value: "", metadata: { colName: "task_completed" } }, 
+    { value: "", metadata: { colName: "task_scheduled_dt" } }, 
+    { value: "", metadata: { colName: "priority_desc" } }
+  ];
+    
+  test("state.Tasks is updated", () => {
+    app.setState({ tasks: [mockTask]});
+    expect(app.state().tasks).toHaveLength(1);
+  });
+      
+  test("the App renders the Task components", () => {
+    // expect number of rendered Task components to equal this.state.tasks array length  
+    app.setState({ isFetched: true });
+    app.update()
+    const tasksContainer = app.find('.tasksContainer');
+
+    expect(tasksContainer.find('.task')).toHaveLength(1);
+  });
+  
 });
