eb05f32f3efa683913c40951ee27c9a1ed86bfde
Ross <RossMorran@hotmail.co.uk>
2020-04-10 23:08:08 +0100

    Add file changes missed on last commit

--- server/routes/addTask.js
+++ server/routes/addTask.js
@@ -1,18 +1,18 @@
-const dbConnection = require("../dbConnection/dbConnection.js").dbConnection;
-const Request = require("tedious").Request;
+// const dbConnection = require("../dbConnection/dbConnection.js").dbConnection;
+// const Request = require("tedious").Request;
 
-exports.addTask = (req, res) => {
-  const { taskTitle } = req.params;
+// exports.addTask = (req, res) => {
+//   const { taskTitle } = req.params;
 
-  const qry =
-    `INSERT INTO tasks (task_id, task_creation_dt, task_title, task_desc, task_completed, task_scheduled_dt, priority_id) ` +
-    `VALUES (NEXT VALUE FOR tasks_seq, GETDATE(), '${taskTitle}', '', 0, GETDATE(), 3);`;
+//   const qry =
+//     `INSERT INTO tasks (task_id, task_creation_dt, task_title, task_desc, task_completed, task_scheduled_dt, priority_id) ` +
+//     `VALUES (NEXT VALUE FOR tasks_seq, GETDATE(), '${taskTitle}', '', 0, GETDATE(), 3);`;
 
-  const request = new Request(qry, err => {
-    if (err) {
-      console.log(err);
-    }
-  });
+//   const request = new Request(qry, err => {
+//     if (err) {
+//       console.log(err);
+//     }
+//   });
 
-  dbConnection(request);
-};
+//   dbConnection(request);
+// };
--- server/routes/amendTask.js
+++ server/routes/amendTask.js
@@ -1,31 +1,31 @@
-const dbConnection = require("../dbConnection/dbConnection.js").dbConnection;
-const Request = require("tedious").Request;
+// const dbConnection = require("../dbConnection/dbConnection.js").dbConnection;
+// const Request = require("tedious").Request;
 
-function getQrySQL({ taskID, fieldName, newValue }) {
-  let qry;
+// function getQrySQL({ taskID, fieldName, newValue }) {
+//   let qry;
 
-  // handle diff data types in qry string
-  if (fieldName === "priority_desc") {
-    qry =
-      `UPDATE tasks SET tasks.priority_id = (SELECT task_priorities.priority_id FROM task_priorities` +
-      ` WHERE task_priorities.priority_desc = '${newValue}') WHERE tasks.task_id = ${taskID};`;
-  } else if (fieldName === "task_completed") {
-    qry = `UPDATE tasks SET task_completed = ${newValue} WHERE task_id = ${taskID};`;
-  } else {
-    qry = `UPDATE tasks SET ${fieldName} = '${newValue}' WHERE task_id = ${taskID};`;
-  }
+//   // handle diff data types in qry string
+//   if (fieldName === "priority_desc") {
+//     qry =
+//       `UPDATE tasks SET tasks.priority_id = (SELECT task_priorities.priority_id FROM task_priorities` +
+//       ` WHERE task_priorities.priority_desc = '${newValue}') WHERE tasks.task_id = ${taskID};`;
+//   } else if (fieldName === "task_completed") {
+//     qry = `UPDATE tasks SET task_completed = ${newValue} WHERE task_id = ${taskID};`;
+//   } else {
+//     qry = `UPDATE tasks SET ${fieldName} = '${newValue}' WHERE task_id = ${taskID};`;
+//   }
 
-  return qry;
-}
+//   return qry;
+// }
 
-exports.amendTask = req => {
-  const qry = getQrySQL(req.params);
+// exports.amendTask = req => {
+//   const qry = getQrySQL(req.params);
 
-  const request = new Request(qry, err => {
-    if (err) {
-      console.log(err);
-    }
-  });
+//   const request = new Request(qry, err => {
+//     if (err) {
+//       console.log(err);
+//     }
+//   });
 
-  dbConnection(request);
-};
+//   dbConnection(request);
+// };
