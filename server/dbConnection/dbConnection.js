e03fbf34c7555e799110b52b94bb7d4feeee3db9
Ross <RossMorran@hotmail.co.uk>
2020-04-10 23:05:58 +0100

    set up initial dev connection to postgres db

--- server/dbConnection/dbConnection.js
+++ server/dbConnection/dbConnection.js
@@ -1,25 +1,35 @@
-const Connection = require("tedious").Connection;
+const { Client } = require("pg");
+const connectionString = require("./connectionString.js").connectionString();
 
-const config = {
-  userName: "ross_wr_54",
-  password: "k32!IW0Nm@HG",
-  server: "tasksappserver.database.windows.net",
-  options: {
-    database: "tasksAppDatabase",
-    encrypt: true,
-    rowCollectionOnRequestCompletion: true
-  }
-};
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || connectionString,
+  ssl: { rejectUnauthorized: false },
+});
 
-exports.dbConnection = request => {
-  const connection = new Connection(config);
+client.connect();
 
-  connection.on("connect", err => {
-    if (err) {
-      console.log(err);
-    } else {
-      connection.execSql(request);
-      connection.close;
-    }
-  });
-};
+client.query("SELECT * FROM tasks;", (err, res) => {
+
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }  
+  client.end();
+});
+
+
+
+// const Connection = require("tedious").Connection;
+
+// exports.dbConnection = request => {
+//   const connection = new Connection(config);
+
+//   connection.on("connect", err => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       connection.execSql(request);
+//       connection.close;
+//     }
+//   });
+// };
