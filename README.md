5e15b830c7837611d735824e8f8ec1ee564554b0
Ross <RossMorran@hotmail.co.uk>
2020-04-13 22:31:50 +0100

    Update README & version numbers

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 A web-based task scheduling app, bootstrapped with Express.js and create-react-app.
 
-The Node.js backend routes HTTP requests from the client to an SQL database hosted on Azure. 
+The Node.js backend routes HTTP requests from the client to a PostgreSQL database set up on Heroku. 
 
 The front-end is written in React.
--- server/dbConnection/dbConnection.js
+++ server/dbConnection/dbConnection.js
@@ -1,11 +1,8 @@
 const { Client } = require("pg");
-// need to take below out of production build
-const connectionString = require("./connectionString.js").connectionString();
 
 exports.dbConnection = (clientRes, qry) => {
   const client = new Client({
-    // need to take connectionString out of production build
-    connectionString: process.env.DATABASE_URL || connectionString,
+    connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
   });
 
