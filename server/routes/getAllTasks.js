4337be5d9c5d45a1b4746d0adc7777c63fe9e833
Ross <RossMorran@hotmail.co.uk>
2020-04-17 23:47:44 +0100

    Add ORDER BY to ensure tasks are rendered in a consistent order

--- server/routes/getAllTasks.js
+++ server/routes/getAllTasks.js
@@ -3,7 +3,7 @@ const dbConnection = require("../dbConnection/dbConnection.js").dbConnection;
 exports.getAllTasks = clientRes => {
   const qry =
     `SELECT task_id, task_creation_dt, task_title, task_desc, task_completed, task_scheduled_dt, priority_desc 
-    FROM tasks LEFT JOIN task_priorities ON tasks.priority_id = task_priorities.priority_id;`; 
+    FROM tasks LEFT JOIN task_priorities ON tasks.priority_id = task_priorities.priority_id ORDER BY task_id;`; 
   
   dbConnection(clientRes, qry);
 };
