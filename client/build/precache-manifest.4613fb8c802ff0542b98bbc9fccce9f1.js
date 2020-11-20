ff571ab98e20726cef1bc87d01d9f8782324686c
Ross <rossmorran@hotmail.co.uk>
2020-11-20 14:43:00 +0000

    Run NPM build after client code changes

---
+++ client/build/precache-manifest.4613fb8c802ff0542b98bbc9fccce9f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c137b14371612222ed2048058c6096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c98f2003835ed001aa90",
+    "url": "/static/css/main.af711991.chunk.css"
+  },
+  {
+    "revision": "0c07d7d6fa9da15274b9",
+    "url": "/static/js/2.fd188335.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd188335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98f2003835ed001aa90",
+    "url": "/static/js/main.484c7039.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
